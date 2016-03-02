ea7fdade294c660c455b763e0731e6b87844406a
Cholathit Khueankaew <kcholathit@gmail.com>
2016-03-03 02:34:59 +0700

    use ENV variable for server address

--- socket.js
+++ socket.js
@@ -3,12 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
 var redis = new Redis();
+
+require('dotenv').config();
+
 redis.psubscribe('*', function(err, count) {
 });
 redis.on('pmessage', function(subscribed, channel, message) {
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
-http.listen(3000, function(){
+http.listen(3000, process.env.SERVER_ADDRESS, function(){
     console.log('Listening on Port 3000');
 });
